91dc641dde8e3e440859239d781ec09ae3cdd544
damon <ldmmdl@163.com>
2016-11-13 10:16:54 +1300

    Add another algorithm for bubble sort function

--- Algorithms/0_BubbleSort/bubble_sort.c
+++ Algorithms/0_BubbleSort/bubble_sort.c
@@ -1,11 +1,14 @@
-// bubble_sort.cpp : 定义控制台应用程序的入口点。
-//
-
-#include "stdafx.h"
+/****************************************
+*           bubble sort
+****************************************/
 
 #include <stdlib.h>  
 #include <stdio.h>  
 
+/*
+ * with each loop of this function, a smallest number will be selected.
+ * and the smallest number will have the index of i.
+ */
 void bubble_sort(int unsorted[],int len)
 {
 	int i, j;
@@ -23,13 +26,39 @@ void bubble_sort(int unsorted[],int len)
 	}
 }
 
+/*
+ * with each loop of this function, a biggest number will be selected.
+ * and the biggest number will have the index of j+1.
+ */
+void bubble_sort_v2(int unsorted[], int len)
+{
+	int i,j;
+	for(i = 0; i < len - 1; ++i) {
+		for(j = i; j < len -1; ++j) {
+			if(unsorted[j] > unsorted[j + 1]) {
+            	unsorted[j] = unsorted[j] ^ unsorted[j + 1]; // use XOR to exchange
+                unsorted[j + 1] = unsorted[j + 1] ^ unsorted[j];
+                unsorted[j] = unsorted[j] ^ unsorted[j + 1];
+			}
+		}
+	}
+}
+
 int main()
 {
-	int x[] = { 6, 2, 4, 1, 5, 9 };
-	bubble_sort(x,6);
+	int x[] = { 6, 2, 4, 1, 4,5, 9 };
+	bubble_sort(x,7);
 	for (int i = 0; i < 6; ++i) {
 		printf("%d ", x[i]);
 	}
-	system("pause");
+	printf("\n");
+	bubble_sort_v2(x, 7);
+	for (int i = 0; i < 6; ++i) {
+
+		printf("%d ", x[i]);
+
+	}
+	printf("\n");
+	return 0;
 }
 
